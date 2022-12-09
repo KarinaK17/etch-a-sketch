45c7e3366553d40ef959db3bd081fb0526486f7f
KarinaK17 <117162205+KarinaK17@users.noreply.github.com>
2022-12-09 13:18:21 +0300

    add buttons to start a new game with different pencils

--- script.js
+++ script.js
@@ -1,15 +1,67 @@
 const container=document.querySelector(".container");
+let pixelsPerSide=16;
 
-let pixelsPerSide=100;
-for (i=0; i<(pixelsPerSide**2); i++) {
-    const pixel=document.createElement("div");
-    pixel.classList.add("pixel");
-    container.appendChild(pixel);
+function game(pixelsPerSide) {
+    for (i=0; i<(pixelsPerSide**2); i++) {
+        const pixel=document.createElement("div");
+        pixel.classList.add("pixel");
+        container.appendChild(pixel);
 
+        pixel.addEventListener("mouseenter", changeColor);
 
-pixel.addEventListener("mouseenter", changeColor);
+        function changeColor() {
+            pixel.style.background="black";
+    }}
+}
 
-function changeColor() {
-    pixel.style.background="black";
-    console.log("hello");
-}}
+game(pixelsPerSide);
+
+const btn=document.querySelector(".btn");
+btn.addEventListener('click', startAgain);
+
+function startAgain() {
+    container.textContent="";
+    pixelsPerSide=prompt("Please enter number of pixels per side for the new game!", 64);
+    pixelsPerSide=Number(pixelsPerSide);
+    container.style.cssText=`grid-template-columns: repeat(${pixelsPerSide}, 1fr)`;
+    game(pixelsPerSide);
+
+    if (pixelsPerSide<0||pixelsPerSide==null||pixelsPerSide>100||!Number.isFinite(pixelsPerSide)) {
+        console.log(typeof pixelsPerSide);
+        alert("Please enter a number from 1 to 100");
+        container.textContent="";
+    }
+}
+
+const btn2=document.querySelector(".btn2");
+btn2.addEventListener('click', startAgain2);
+
+function startAgain2() {
+    container.textContent="";
+    pixelsPerSide=prompt("Please enter number of pixels per side for the new game!", 64);
+    pixelsPerSide=Number(pixelsPerSide);
+    container.style.cssText=`grid-template-columns: repeat(${pixelsPerSide}, 1fr)`;
+    function game(pixelsPerSide) {
+        for (i=0; i<(pixelsPerSide**2); i++) {
+            const pixel=document.createElement("div");
+            pixel.classList.add("pixel");
+            container.appendChild(pixel);
+    
+            pixel.addEventListener("mouseenter", changeColor);
+    
+            function changeColor(e) {
+            /* beautiful purple and blue pencil 
+            pixel.style.background="rgb("+(e.clientX/3)+","+(e.clientY/3)+", "+(e.clientX/2)+")";
+            */
+            // random color pencil
+            pixel.style.background="rgb("+Math.floor(Math.random() * 256)+","+Math.floor(Math.random() * 256)+", "+Math.floor(Math.random() * 256)+")";
+        }}
+    }
+    game(pixelsPerSide);
+
+    if (pixelsPerSide<0||pixelsPerSide==null||pixelsPerSide>100||!Number.isFinite(pixelsPerSide)) {
+        console.log(typeof pixelsPerSide);
+        alert("Please enter a number from 1 to 100");
+        container.textContent="";
+    }
+}
